45c79d66f2c7e82fff285f5c58f2e6d86a19eed6
alvaropinot <alvaro@neatnait.com>
2015-04-09 17:05:43 +0200

    added last user scanned info

--- client/app/trials/trials.detail.controller.js
+++ client/app/trials/trials.detail.controller.js
@@ -9,15 +9,52 @@ angular.module('puebloMalditoWebappApp')
       });
     });
 
+    $scope.usersPassedPercent = function () {
+      if($scope.trial){
+        return $scope.trial.users.length*100/400;
+      }
+      else{
+        return 0;
+      }
+    };
+
+    $scope.cleanErrors = function () {
+      $scope.errors = {};
+    };
+
+    $scope.cleanErrors();
+
     $scope.add = function(form) {
       $scope.submitted = true;
       if(form.$valid) {
-        User.addTrial({ id: $scope.code }, {trialId: $stateParams.trialId}, function(){
+
+        $scope.lastUserScanned = false;
+
+        User.addTrial({ id: $scope.code }, {trialId: $stateParams.trialId}, function(data){
+
+          $scope.cleanErrors();
+
           $scope.code = '';
+          $scope.lastUserScanned = data.user;
+
+          //first time passed trial
+          if(data.trial){
+            $scope.trial = data.trial;
+          }
+          else{
+            $scope.errors.userNotChanged = true;
+          }
+
           $scope.submitted = false;
-        }, function(){
+
+        }, function(err){
           // TODO : Show errors
           //if(404)
+
+          /*if(err.status === 304){
+            $scope.errors.userNotChanged = true;
+          }*/
+          console.log(err);
         });
       }
     };
