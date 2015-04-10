300a43e68183e52de4c8eb7630f8b9c18f4281d9
RubenNN <ruben@neatnait.com>
2015-04-10 15:54:32 +0200

    Profile with trials passed

--- client/app/profiles/profiles.controller.js
+++ client/app/profiles/profiles.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('puebloMalditoWebappApp')
-  .controller('ProfilesCtrl', function ($scope, $stateParams, User) {
-    $scope.publicProfile = User.getByCode({ controller: $stateParams.code });
+  .controller('ProfilesCtrl', function ($scope, $stateParams, User, Trial) {
+  	$scope.trials = [];
+    $scope.publicProfile = User.getByCode({ controller: $stateParams.code }, function(){
+    	angular.forEach($scope.publicProfile.trials, function(value, key) {
+    		Trial.get({ id: value.trial }, function(a) {
+					$scope.trials.push(a);
+				});
+			});
+    });
+
+    
   });
