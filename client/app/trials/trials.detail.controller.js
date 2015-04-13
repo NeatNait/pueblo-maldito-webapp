d701c16bc598768b383d03858a2a408c05586030
alvaropinot <alvaro@neatnait.com>
2015-04-13 15:02:38 +0200

    WIP control digit

--- client/app/trials/trials.detail.controller.js
+++ client/app/trials/trials.detail.controller.js
@@ -59,4 +59,41 @@ angular.module('puebloMalditoWebappApp')
       }
     };
 
+
+    function calculateDigit(number){
+
+      var numbers     = _.map((number+'').split(''), Number),//to string split and back to number
+          oddsEvens   = _.partition(numbers, evenPosition),
+          oddsSum     = _.sum(oddsEvens[1]) || 0, //prevent NaN for 1 digit numbers
+          evensSum    = _.sum(oddsEvens[0]),
+          result      = (evensSum*3 + oddsSum) % 10;
+
+      return result ? 10-result : result;
+
+      function evenPosition(num, i){
+        return i % 2 == 0;
+      }
+    }
+
+    function checkDigit(number){
+      var numberString   = number+'',
+          length         = numberString.length,
+          originalNumber = _.parseInt(numberString.substr(0, length-1)),
+          controlDigit   = _.parseInt(numberString.substr(length-1, length));
+
+      return calculateDigit(originalNumber) === controlDigit;
+    }
+
+    /*for (var i = 0; i < 700; i++) {
+      console.log('input', i);
+      var n = _.parseInt(i+''+calculateDigit(i));
+      console.log(n);
+      console.log(i+''+checkDigit(n));
+    };*/
+
+    for (var i = 0; i < 700; i++) {
+      console.log('input', i);
+      console.log(i+''+checkDigit(i));
+    };
+
   });
