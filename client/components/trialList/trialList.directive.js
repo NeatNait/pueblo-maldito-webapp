f5e125ffa3a6d46bc765712584441906fd345300
alvaropinot <alvaro@neatnait.com>
2015-04-13 15:01:51 +0200

    allow delete in trialList

--- client/components/trialList/trialList.directive.js
+++ client/components/trialList/trialList.directive.js
@@ -9,6 +9,16 @@ angular.module('puebloMalditoWebappApp')
         allowDelete: '=allowDelete',
         trials: '=trials'
       },
+      controller: function($scope, $element, Trial){
+        $scope.delete = function(trial) {
+          Trial.remove({ id: trial._id });
+          angular.forEach($scope.trials, function(u, i) {
+            if (u === trial) {
+              $scope.trials.splice(i, 1);
+            }
+          });
+        };
+      },
       link: function (scope, element, attrs) {
       }
     };
