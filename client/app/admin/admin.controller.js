54197b9c81ff67095833aa462031ab9734309223
RubenNN <ruben@neatnait.com>
2015-04-14 09:00:07 +0200

    Confirm delete users

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('puebloMalditoWebappApp')
-  .controller('AdminCtrl', function ($scope, $http, Auth, User) {
+  .controller('AdminCtrl', function ($scope, $http, $window, Auth, User) {
 
     // Use the User $resource to fetch all users
     $scope.users = User.query(function(users){
@@ -15,12 +15,16 @@ angular.module('puebloMalditoWebappApp')
 
 
     $scope.delete = function(user) {
-      User.remove({ id: user._id });
-      angular.forEach($scope.users, function(u, i) {
-        if (u === user) {
-          $scope.users.splice(i, 1);
-        }
-      });
+      var deleteUser = $window.confirm('Are you absolutely sure you want to delete?');
+      if (deleteUser) {
+        User.remove({ id: user._id });
+          angular.forEach($scope.users, function(u, i) {
+          if (u === user) {
+            $scope.users.splice(i, 1);
+          }
+        });
+      }
+      
     };
 
     $scope.user = {};
