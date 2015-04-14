f5f4a9273bb03c8d10df0a699acbeba51125a7db
alvaropinot <alvaro@neatnait.com>
2015-04-14 13:49:37 +0200

    create user endpoint now requires admin

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -12,7 +12,7 @@ router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
 router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', controller.create);
+router.post('/', auth.hasRole('admin'), controller.create);
 
 // TODO : change admin for staff
 router.put('/:code/trial/complete', auth.hasRole('admin'), controller.completeTrial);
