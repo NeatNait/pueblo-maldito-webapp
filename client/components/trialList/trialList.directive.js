90de7ff7975d95471709c5b7a9b95aaf58b75b58
RubenNN <ruben@neatnait.com>
2015-04-14 09:00:22 +0200

    Confirm delete trials

--- client/components/trialList/trialList.directive.js
+++ client/components/trialList/trialList.directive.js
@@ -9,15 +9,18 @@ angular.module('puebloMalditoWebappApp')
         allowDelete: '=allowDelete',
         trials: '=trials'
       },
-      controller: function($scope, $element, Trial){
+      controller: function($scope, $element, $window, Trial){
         $scope.delete = function(trial) {
-          Trial.remove({ id: trial._id });
-          angular.forEach($scope.trials, function(u, i) {
-            if (u === trial) {
-              $scope.trials.splice(i, 1);
-            }
-          });
-        };
+          var deleteTrial = $window.confirm('Are you absolutely sure you want to delete?');
+          if (deleteTrial) {
+            Trial.remove({ id: trial._id });
+            angular.forEach($scope.trials, function(u, i) {
+              if (u === trial) {
+                $scope.trials.splice(i, 1);
+              }
+            });
+          };
+        }
       },
       link: function (scope, element, attrs) {
       }
