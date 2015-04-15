def90d68cf1a12aa798e8a928aac3f60c6f87932
alvaropinot <alvaro@neatnait.com>
2015-04-15 13:14:55 +0200

    ADD trial - allow save and update

--- client/app/admin/trial/trial.controller.js
+++ client/app/admin/trial/trial.controller.js
@@ -4,28 +4,40 @@ angular.module('puebloMalditoWebappApp')
   .controller('AdminTrialCtrl', function ($scope, Trial) {
 
     $scope.trials = Trial.query();
-    $scope.newTrial = new Trial();
+    $scope.trial = new Trial();
 
     $scope.add = function(form) {
-      if($scope.newTrial === '') {
+      if($scope.trial === '') {
         return;
       }
 
       $scope.submitted = true;
 
       if(form.$valid) {
-        $scope.newTrial.$save(function(){
-          $scope.trials.push(angular.copy($scope.newTrial));
+        $scope.trial.$save(function(){
+
+          var t = _.find($scope.trials, function(trial){
+            return trial._id === $scope.trial._id;
+          });
+
+          if(!t){
+            $scope.trials.push(angular.copy($scope.trial));
+          }
+
           $scope.submitted = false;
-          $scope.newTrial = new Trial();
+          $scope.trial = new Trial();
         });
       }
 
     };
 
     $scope.delete = function(trial) {
-      trial.$delete();
-      $scope.trials = _.without($scope.trials, trial);
+      $scope.trial = new Trial();
+      //$scope.trials = _.without($scope.trials, trial);
+    };
+
+    $scope.edit = function (trial) {
+      $scope.trial = trial;
     };
 
   });
