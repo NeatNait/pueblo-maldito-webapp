58b1ea3d41f1347c4cf6803029bbf598c1c51bcf
alvaropinot <alvaro@neatnait.com>
2015-04-16 20:20:23 +0200

    REFACTOR trial.detail to use userCodeManager

--- client/app/trials/trials.detail.controller.js
+++ client/app/trials/trials.detail.controller.js
@@ -4,9 +4,9 @@
   angular.module('puebloMalditoWebappApp')
     .controller('TrialsDetailCtrl', TrialsDetailCtrl);
 
-  TrialsDetailCtrl.$inject = ['$stateParams', 'trials', 'User'];
+  TrialsDetailCtrl.$inject = ['$stateParams', 'trials', 'User', 'userCodeManager'];
 
-  function TrialsDetailCtrl($stateParams, trials, User) {
+  function TrialsDetailCtrl($stateParams, trials, User, userCodeManager) {
 
     var vm = this;
 
@@ -96,40 +96,18 @@
       }
     }
 
-    function calculateDigit(number){
 
-      var numbers   = _.map((number+'').split(''), Number),//to string split and back to number
-          oddsEvens = _.partition(numbers, evenPosition),
-          oddsSum   = _.sum(oddsEvens[1]) || 0, //prevent NaN for 1 digit numbers
-          evensSum  = _.sum(oddsEvens[0]),
-          result    = (evensSum*3 + oddsSum) % 10;
-
-      return result ? 10-result : result;
-
-      function evenPosition(num, i){
-        return i % 2 == 0;
-      }
-    }
-
-    function checkDigit(number){
-      var numberString   = number+'',
-          length         = numberString.length,
-          originalNumber = _.parseInt(numberString.substr(0, length-1)),
-          controlDigit   = _.parseInt(numberString.substr(length-1, length));
-
-      return calculateDigit(originalNumber) === controlDigit;
-    }
 
     /*for (var i = 0; i < 900; i++) {
       //console.log('input', i);
-      var n = _.parseInt(i+''+calculateDigit(i));
+      var n = _.parseInt(i+''+ userCodeManager.calculateDigit(i));
       console.log(n);
-      //console.log(i+''+checkDigit(n));
+      //console.log(i+''+ userCodeManager.checkDigit(n));
     }*/
 
     /*for (var i = 0; i < 700; i++) {
       console.log('input', i);
-      console.log(i+''+checkDigit(i));
+      console.log(i+''+ userCodeManager.checkDigit(i));
     };*/
 
   }
