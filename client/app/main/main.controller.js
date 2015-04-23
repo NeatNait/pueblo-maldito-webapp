2748fee5a891414f35afb556b0ad50ce045fb8db
alvaropinot <alvaro@neatnait.com>
2015-04-23 13:01:09 +0200

    DRAF main-view imported

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,68 +2,288 @@
 
 angular.module('puebloMalditoWebappApp')
   .controller('MainCtrl', function ($scope, $http, socket, $interval) {
-    $scope.awesomeThings = [];
+    var src = $('.cover-img').css('background-image');
+    var url = src.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    var img = new Image();
+    img.onload = function() {
+        //alert('image loaded');
+        $('.js-wait-for-image').toggleClass('js-wait-for-image animation');
+    };
+    img.src = url;
+    if (img.complete){
+      img.onload();
+    }
+
+
+
+    $(function() {
+
+      $('.modal').on('shown.bs.modal', function() {
+          $(this).find('iframe').attr('src','http://entradium.com/entradas/3364.widget');
+      });
+
+      //smooth scrolling
+      //$('a[href^="#"]').on('click', function (e) {
+      $('a.page-scroll').on('click', function (e) {
+          e.preventDefault();
+
+          var target = this.hash,
+          $target = $(target),
+          marginTop = 80;
+
+          //window.location.hash = target;
+
+          $('html, body').stop().animate({
+              'scrollTop': $target.offset().top - marginTop
+          }, 1000, 'swing', function () {
+              //window.location.hash = target;
+              if(history.pushState) {
+                history.pushState(null, null, target);
+              }
+              else {
+                window.location.hash = target;
+              }
+          });
+      });
+
+      // Highlight the top nav as scrolling occurs
+      $('body').scrollspy({
+          target: '.navbar-fixed-top'
+      });
+
+      // Closes the Responsive Menu on Menu Item Click
+      $('.navbar-collapse ul li a').click(function() {
+          $('.navbar-toggle:visible').click();
+      });
+
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    /*$(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });*/
+
+
+
+    /**
+     * cbpAnimatedHeader.js v1.0.0
+     * http://www.codrops.com
+     *
+     * Licensed under the MIT license.
+     * http://www.opensource.org/licenses/mit-license.php
+     *
+     * Copyright 2013, Codrops
+     * http://www.codrops.com
+     */
+    var cbpAnimatedHeader = (function() {
+
+      var docElem = document.documentElement,
+        header = document.querySelector( '.navbar' ),
+        didScroll = false,
+        changeHeaderOn = 100;
+
+      function init() {
+        window.addEventListener( 'scroll', function( event ) {
+          if( !didScroll ) {
+            didScroll = true;
+            setTimeout( scrollPage, 250 );
+          }
+        }, false );
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+
+      function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+          classie.add( header, 'navbar-shrink' );
+        }
+        else {
+          classie.remove( header, 'navbar-shrink' );
+        }
+        didScroll = false;
+      }
+
+      function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+      }
+
+      init();
+
+    })();
+
+
+    /*!
+     * classie - class helper functions
+     * from bonzo https://github.com/ded/bonzo
+     *
+     * classie.has( elem, 'my-class' ) -> true/false
+     * classie.add( elem, 'my-new-class' )
+     * classie.remove( elem, 'my-unwanted-class' )
+     * classie.toggle( elem, 'my-class' )
+     */
+
+    /*jshint browser: true, strict: true, undef: true */
+    /*global define: false */
+
+    ( function( window ) {
+
+    'use strict';
+
+    // class helper functions from bonzo https://github.com/ded/bonzo
+
+    function classReg( className ) {
+      return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+    }
+
+    // classList support for class management
+    // altho to be fair, the api sucks because it won't accept multiple classes at once
+    var hasClass, addClass, removeClass;
+
+    if ( 'classList' in document.documentElement ) {
+      hasClass = function( elem, c ) {
+        return elem.classList.contains( c );
+      };
+      addClass = function( elem, c ) {
+        elem.classList.add( c );
+      };
+      removeClass = function( elem, c ) {
+        elem.classList.remove( c );
+      };
+    }
+    else {
+      hasClass = function( elem, c ) {
+        return classReg( c ).test( elem.className );
+      };
+      addClass = function( elem, c ) {
+        if ( !hasClass( elem, c ) ) {
+          elem.className = elem.className + ' ' + c;
+        }
+      };
+      removeClass = function( elem, c ) {
+        elem.className = elem.className.replace( classReg( c ), ' ' );
+      };
+    }
+
+    function toggleClass( elem, c ) {
+      var fn = hasClass( elem, c ) ? removeClass : addClass;
+      fn( elem, c );
+    }
+
+    var classie = {
+      // full names
+      hasClass: hasClass,
+      addClass: addClass,
+      removeClass: removeClass,
+      toggleClass: toggleClass,
+      // short names
+      has: hasClass,
+      add: addClass,
+      remove: removeClass,
+      toggle: toggleClass
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    // transport
+    if ( typeof define === 'function' && define.amd ) {
+      // AMD
+      define( classie );
+    } else {
+      // browser global
+      window.classie = classie;
+    }
 
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
+    })( window );
 
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
-    $scope.onClick = function (points, evt) {
-      console.log(points, evt);
-    };
 
-    $scope.labelsDoughnut = ["Alive", "Unknow", "Dead"];
-    $scope.dataDoughnut = [800, 100, 200];
 
-    $scope.labelsBars = ['Trial 06', 'Trial 07', 'Trial 08', 'Trial 09', 'Trial 10', 'Trial 11', 'Trial 12','Trial 06', 'Trial 07', 'Trial 08', 'Trial 09', 'Trial 10', 'Trial 11', 'Trial 12','Trial 06', 'Trial 07', 'Trial 08', 'Trial 09', 'Trial 10', 'Trial 11', 'Trial 12'];
-    $scope.seriesBars = ['Passed', 'Failed'];
 
-    $scope.dataBars = [
-      [65, 59, 80, 81, 56, 55, 22, 32, 14, 44, 43, 55, 33, 25, 55, 65, 59, 80, 81, 56, 55],
-      [28, 48, 40, 19, 86, 27, 42, 22, 11, 23, 12, 22, 28, 40, 86, 27, 42, 22, 11, 23, 12]
-    ];
 
-    $scope.coloursBars = [
-      //'#97BBCD', // blue
-      //'#DCDCDC', // light grey
-      '#F7464A', // red
-      '#46BFBD', // green
-      '#FDB45C', // yellow
-      '#949FB1', // grey
-      '#4D5360'  // dark grey
-    ];
-
-    $interval(function(){
-      //$scope.dataDoughnut = [Math.random()*400, Math.random()*400, Math.random()*400];
-      $scope.dataDoughnut[0]*=0.9;
-      $scope.dataDoughnut[2]*=1.1;
-      $scope.dataDoughnut[1]*=1.05;
-      /*$scope.data = [
-        [Math.random()*100, Math.random()*100, Math.random()*100],
-        [Math.random()*100, Math.random()*100, Math.random()*100]
-      ];*/
-    }, 5000);
   });
+
+
+
+(function(){
+  'use strict';
+
+  /* recommended */
+  angular
+    .module('puebloMalditoWebappApp')
+    .config(TrialConfig);
+
+  TrialConfig.$inject = ['$provide'];
+
+  function TrialConfig($provide) {
+    $provide.decorator('Trial', trialDecorator);
+  }
+
+  trialDecorator.$inject = ['$delegate'];
+
+  function trialDecorator($delegate) {
+
+    return
+    return function(exception, cause) {
+      $delegate(exception, cause);
+      var errorData = {
+          exception: exception,
+          cause: cause
+      };
+      /**
+       * Could add the error to a service's collection,
+       * add errors to $rootScope, log errors to remote web server,
+       * or log locally. Or throw hard. It is entirely up to you.
+       * throw exception;
+       */
+      //toastr.error(exception.msg, errorData);
+      alert(exception.msg);
+      alert(errorData.exception);
+      //$('body').html(errorData.exception);
+    };
+  }
+})();
+
+
+
+(function(){
+    'use strict';
+
+  /* recommended */
+  angular
+    .module('puebloMalditoWebappApp')
+    .config(exceptionConfig);
+
+  exceptionConfig.$inject = ['$provide'];
+
+  function exceptionConfig($provide) {
+    $provide.decorator('$exceptionHandler', extendExceptionHandler);
+  }
+
+  extendExceptionHandler.$inject = ['$delegate'];
+
+  function extendExceptionHandler($delegate) {
+    return function(exception, cause) {
+      $delegate(exception, cause);
+      var errorData = {
+          exception: exception,
+          cause: cause
+      };
+      /**
+       * Could add the error to a service's collection,
+       * add errors to $rootScope, log errors to remote web server,
+       * or log locally. Or throw hard. It is entirely up to you.
+       * throw exception;
+       */
+      //toastr.error(exception.msg, errorData);
+      alert(exception.msg);
+      alert(errorData.exception);
+      //$('body').html(errorData.exception);
+    };
+  }
+})();
+
+
+
