6e6e79ad4f5095c7643815049bea86d4b843587a
carlosbermejonn <carlos@neatnait.com>
2015-04-24 22:14:05 +0200

    prensa & imagenes routes

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@
 'use strict';
 
 var errors = require('./components/errors');
+var express = require('express');
 
 module.exports = function(app) {
 
@@ -14,10 +15,14 @@ module.exports = function(app) {
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
+  app.use('/prensa',express.static('prensa'));
+  app.use('/imagenes',express.static('imagenes'));
+
+
   app.use('/auth', require('./auth'));
-  
+
   // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+  app.route('/:url(prensa|api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
   // All other routes should redirect to the index.html
