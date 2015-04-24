aad0cf291770dacebf85682f54383f44a2586129
alvaropinot <alvaro@neatnait.com>
2015-04-24 20:52:29 +0200

    redirect to trials on login

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -15,7 +15,7 @@ angular.module('puebloMalditoWebappApp')
         })
         .then( function() {
           // Logged in, redirect to home
-          $location.path('/');
+          $location.path('/trials');
         })
         .catch( function(err) {
           $scope.errors.other = err.message;
