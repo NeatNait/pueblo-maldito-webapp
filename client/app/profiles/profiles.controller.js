2e821a5b07993d4ad66718aea8e143dead54d2c2
carlosbermejonn <carlos@neatnait.com>
2015-04-25 13:42:16 +0200

    visual profile updates

--- client/app/profiles/profiles.controller.js
+++ client/app/profiles/profiles.controller.js
@@ -2,14 +2,33 @@
 
 angular.module('puebloMalditoWebappApp')
   .controller('ProfilesCtrl', function ($scope, $stateParams, User) {
+
   	$scope.trials = [];
+    $scope.awards = [];
+    $scope.deaths = [];
 
     $scope.publicProfile = User.getByCode({ controller: $stateParams.code }, function(){
+
+      //console.log($scope.publicProfile);
+
     	angular.forEach($scope.publicProfile.trials, function(value, key) {
-			 $scope.trials.push(value.trial);
-			});
+
+        switch(value.trial.type){
+          case 'Trial':
+            $scope.trials.push(value.trial);
+            break;
+          case 'Award':
+            $scope.awards.push(value.trial);
+            break;
+          case 'Death':
+            $scope.deaths.push(value.trial);
+            break;
+        }
+
+        //$scope.trials.push(value.trial);
+
+
+		  });
     });
 
-    
-    
   });
